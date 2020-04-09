79ce8a5fee839e515ff961a71b484591ed13b407
yukirito97 <www.wyuxin@qq.com>
2020-04-09 13:34:45 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/wyx/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyx.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/wyx/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyx.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
